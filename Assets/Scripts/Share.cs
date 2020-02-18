3a75cec7d91838ff1d84b8ba5e4188f2c8ea3a8d
0xSeb <sebastien.marchal@protonmail.com>
2020-02-18 17:46:44 +0100

    [RELEASE] Share fonctionnel

---
+++ Assets/Scripts/Share.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Share : MonoBehaviour
+{
+    private NativeShare _nativeShare;
+
+    private void Start()
+    {
+        _nativeShare = new NativeShare();
+    }
+
+    public void SharePicture(string pictureName)
+    {
+        
+    }
+    
+}
---
+++ Assets/Scripts/TakeScreenshot.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class TakeScreenshot : MonoBehaviour
+{
+
+    [SerializeField] private GameObject _canvas;
+    
+    public void TakePicture()
+    {
+        StartCoroutine(TakeSSAndShare());
+    }
+    
+    
+    private IEnumerator TakeSSAndShare()
+    {
+        yield return new WaitForEndOfFrame();
+
+        //Texture2D ss = new Texture2D( Screen.width, Screen.height, TextureFormat.RGB24, false );
+        Texture2D ss = ScreenCapture.CaptureScreenshotAsTexture();
+        //ss.ReadPixels( new Rect( 0, 0, Screen.width, Screen.height ), 0, 0 );
+        //ss.Apply();
+
+        string filePath = Path.Combine( Application.temporaryCachePath, "img.png" );
+        File.WriteAllBytes( filePath, ss.EncodeToPNG() );
+	
+        // To avoid memory leaks
+        Destroy( ss );
+
+        new NativeShare().AddFile( filePath ).SetSubject( "Subject goes here" ).SetText( "Hello world!" ).Share();
+
+        // Share on WhatsApp only, if installed (Android only)
+        //if( NativeShare.TargetExists( "com.whatsapp" ) )
+        //	new NativeShare().AddFile( filePath ).SetText( "Hello world!" ).SetTarget( "com.whatsapp" ).Share();
+    }
+}
